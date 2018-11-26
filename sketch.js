34af553577f5f3cb6a115c63fc2132e010174e00
vibravo <45087542+vibravo@users.noreply.github.com>
2018-11-26 13:31:06 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var bubble = {
+	x: 400,
+	y: 400,
+	radius:100,
+
+};
+	
+var color = {
+	r: 255,
+	g: 204,
+	b: 204,
+
+};
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+	background(0);
+}
+
+function draw() {
+	noFill();
+	strokeWeight(2);
+	display(bubble);
+	
+}
+
+function display() {
+	color.r = random(100,255);
+	color.g = 0
+	color.b = random(100,190);
+	bubble.x = random(0,width);
+	bubble.y = random(0,height);
+	bubble.radius = random (5,150);
+	
+	stroke(color.r,color.g,color.b);
+	ellipse(bubble.x,bubble.y,bubble.radius,bubble.radius);
+}
